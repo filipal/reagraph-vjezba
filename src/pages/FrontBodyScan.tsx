0dee9f9a990203e58c9df006f38a32e1a4c58bf8
filipal <posao.kalendar@gmail.com>
2025-08-06 11:21:22 +0200

    sitni prepravci scss za frontscan

--- src/pages/FrontBodyScan.tsx
+++ src/pages/FrontBodyScan.tsx
@@ -97,6 +97,9 @@ export default function FrontBodyScan({ onClose, onContinueToSideScan }: { onClo
 
   return (
     <div className={styles.frontBodyScanPage}>
+      {scanPhase === 'initial' && (
+        <img src={WomanFront} alt="Woman front" className={styles.backgroundImage} />
+      )}
       <video ref={videoRef} className={styles.video} autoPlay playsInline />
       <Header
         className={styles.scanHeader}
@@ -112,9 +115,6 @@ export default function FrontBodyScan({ onClose, onContinueToSideScan }: { onClo
 
       {/* Main scan area */}
       <div className={styles.scanArea}>
-        {scanPhase === 'initial' && (
-          <img src={WomanFront} alt="Woman front" className={styles.womanFront} />
-        )}
         {scanPhase !== 'initial' && (
           <div className={styles.overlay}>
             <img src={FrontGuide} alt="Front guide" className={styles.frontGuide} />
