2edb9a87ef6b50b3bb47e9adecbffa7d8d8a3818
filipal <posao.kalendar@gmail.com>
2025-08-24 22:50:12 +0200

    pokušaj implementacije AWS - neuspješno

--- src/oidcConfig.ts
+++ src/oidcConfig.ts
@@ -1,7 +1,7 @@
 const oidcConfig = {
   authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_0cK7yNVJr",
   client_id: "35gs2safccnf49vo9d7ubqv65o",
-  redirect_uri: window.location.origin,
+  redirect_uri: `${window.location.origin}/login`,
   response_type: "code",
   scope: "openid email phone",
 }
